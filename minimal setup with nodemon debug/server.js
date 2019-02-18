b5615b8109bba504f3424dd3af1e4519ebd7f7f9
Stefan Uzunov <stefan.uzunov@spark-vision.com>
2019-02-19 00:47:46 +0200

    Add example of minimal debuggable node server

---
+++ minimal setup with nodemon debug/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 3062;
+const app = express();
+
+app.listen(port, function () {
+    console.log("Server is running on " + port + " port");
+});
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>')
+})
